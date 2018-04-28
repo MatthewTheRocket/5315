24ac2dcf1a56316b0af71c4d34cdf61936406672
matt <ensslenm@seattleu.edu>
2018-04-28 13:09:25 -0700

    hw files

---
+++ HW2data.js
@@ -0,0 +1,47 @@
+{
+  "data": [
+    {
+      "name": "Kevin Kerr",
+      "age":"31",
+      "favorite food": "Pizza"
+    },
+     {
+      "name": "Ben McCarty",
+      "age":"31",
+      "favorite food": "Donuts"
+    },
+      {
+      "name": "Peter Demers",
+      "age":"30",
+      "favorite food": "Sushi"
+    },  {
+      "name": "Gerry Barthelmes",
+      "age":"30",
+      "favorite food": "Peanuts"
+    },  {
+      "name": "Parker Faith",
+      "age":"34",
+      "favorite food": "Tacos"
+    },  {
+      "name": "Ben Bishop",
+      "age":"38",
+      "favorite food": "Biscuits"
+    },  {
+      "name": "Matt Wood",
+      "age":"35",
+      "favorite food": "Meatloaf"
+    },  {
+      "name": "Chris Killmer",
+      "age":"32",
+      "favorite food": "Cake"
+    },  {
+      "name": "Jaimee Johnson",
+      "age":"28",
+      "favorite food": "Pizza"
+    },  {
+      "name": "Luke Just",
+      "age":"32",
+      "favorite food": "Spam"
+    }
+  ]
+}
